968696420d14efff24214942cdf639fb9b8ac3bc
chris <christcrom988@gmail.com>
2022-02-10 17:12:23 -0600

    Servlet Basic Example

--- MyServletProject/src/main/java/MyServlet.java
+++ MyServletProject/src/main/java/MyServlet.java
@@ -1,5 +1,4 @@
 
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,40 +6,33 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import org.apache.jasper.tagplugins.jstl.core.Out;
 
-
-/**
- * Servlet implementation class MyServlet
- */
-@WebServlet("/MyServlet")
+@WebServlet(name = "MyServlet", description = "Servlet with Annotation", urlPatterns = { "/MyServlet", "/HellowWorld" })
 public class MyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MyServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public MyServlet() {
+		super();
+
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		PrintWriter writer = response.getWriter();
+		writer.println("Hello person ");
+		int i = 4;
+		writer.println(i * 15);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-		
+
 	}
 
 }
